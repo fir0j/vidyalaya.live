47964abe99a5df511d408e2927a65111fc03523f
fir0j <firoj.is.available@gmail.com>
2020-05-13 21:17:40 +0530

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
-import { Homepage } from './components/Homepage.component';
+// import { Homepage } from './components/Homepage.component';
 import { Topbar } from './components/Topbar.component';
+import { Sidebar } from './components/Sidebar.component';
 
 const App = () => {
-	const Sidebar = () => <div>Sidebar</div>;
 	return (
 		<Fragment>
+			<Topbar />
 			<Sidebar />
 		</Fragment>
 	);
---
+++ src/components/Sidebar.component.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+export const Sidebar = () => (
+	<div className="border sidebar flex flex-col flex-start px-8">
+		<div className="h-16 flex-center sidebar-item mt-15px ">
+			<div className="flex-none flex w-full ">
+				<p className="flex-none img-school w-6 h-6 flex" />
+				<p className="flex-none sidebar-text ml-5px">My Classroom</p>
+			</div>
+		</div>
+
+		<div className=" w-full h-16 flex-center justify-start sidebar-category">
+			<p className=""> MANAGE</p>
+		</div>
+
+		<div className="h-16 flex-center sidebar-item ">
+			<div className="flex-none flex w-full">
+				<p className="flex-none img-video w-6 h-6 flex" />
+				<p className="flex-none sidebar-text ml-5px">Videos</p>
+			</div>
+		</div>
+
+		<div className="h-16 flex-center sidebar-item ">
+			<div className="flex-none flex w-full">
+				<p className="flex-none img-assignment w-6 h-6 flex" />
+				<p className="flex-none sidebar-text ml-5px">Assignments</p>
+			</div>
+		</div>
+
+		<div className="h-16 flex-center sidebar-item ">
+			<div className="flex-none flex w-full">
+				<p className="flex-none img-note w-6 h-6 flex" />
+				<p className="flex-none sidebar-text ml-5px">Notes</p>
+			</div>
+		</div>
+
+		<div className=" w-full h-16 flex-center justify-start sidebar-category">
+			<p className=""> OTHERS</p>
+		</div>
+
+		<div className="h-16 flex-center sidebar-item ">
+			<div className="flex-none flex w-full">
+				<p className="flex-none img-setting w-6 h-6 flex" />
+				<p className="flex-none sidebar-text ml-5px">Settings</p>
+			</div>
+		</div>
+
+		<div className="h-16 flex-center sidebar-item ">
+			<div className="flex-none flex w-full">
+				<p className="flex-none img-contact w-6 h-6 flex" />
+				<p className="flex-none sidebar-text ml-5px">Contact us</p>
+			</div>
+		</div>
+
+		<div className="h-16 flex-center sidebar-item ">
+			<div className="flex-none flex w-full">
+				<p className="flex-none img-faq w-6 h-6 flex" />
+				<p className="flex-none sidebar-text ml-5px">FAQ</p>
+			</div>
+		</div>
+	</div>
+);
