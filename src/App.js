8ee4dc21e70a7d519f0eb2652efb4da5cf4e4413
fir0j <firoj.is.available@gmail.com>
2020-05-13 17:50:01 +0530

    done with Topbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Fragment } from 'react';
 import { Homepage } from './components/Homepage.component';
+import { Topbar } from './components/Topbar.component';
 
 const App = () => {
-	const Topbar = () => (
-		<div className="border w-full bg-brand-blue text-gray-100 h-16 flex  items-center pl-5 text-3xl">
-			<p style={{ marginRight: 'auto' }}>Vidyalaya</p>
-			<div className="w-1/3 flex justify-around">
-				<p className="flex-none">CONTACT</p>
-				<p className="flex-none">LOGIN</p>
-			</div>
-		</div>
-	);
-
+	const Sidebar = () => <div>Sidebar</div>;
 	return (
 		<Fragment>
-			<Homepage />
+			<Sidebar />
 		</Fragment>
 	);
 };
---
+++ src/components/Topbar.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Topbar = () => (
+	<div className="flex-center">
+		<div className="navbar h-32 w-full flex justify-center items-center landscape:px-4 md:px-6 xl:px-8">
+			<div className="relative flex w-full flex justify-between items-center">
+				<p className="logo mr-2 xl:ml-8 landscape:mr-0">Vidyalaya</p>
+				<div className="login-container flex lg:flex-none justify-around ">
+					<p className="flex-none contact">CONTACT</p>
+					<p className="flex-none login">LOGIN</p>
+				</div>
+			</div>
+		</div>
+	</div>
+);
