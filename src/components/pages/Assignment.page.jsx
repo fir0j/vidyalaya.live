021568674174d2cef1002c6d3dfdd09a615af1cb
fir0j <firoj.is.available@gmail.com>
2020-05-15 15:31:49 +0530

    added dumb contents to each page

--- src/components/pages/Assignment.page.jsx
+++ src/components/pages/Assignment.page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export const AssignmentPage = () => (
 	<div className="mt-8 w-full">
@@ -30,52 +30,33 @@ export const AssignmentPage = () => (
 			</div>
 			<hr />
 
-			<div className=" contents w-full h-screen flex justify-start items-center">
-				<div className=" flex justify-start w-full">
-					<p>Write an Essay</p>
-				</div>
-				<div className=" flex justify-start w-full">
-					<p>May 10, 2020</p>
-				</div>
-				<div className=" flex justify-start w-full">
-					<p>May 10, 2020 1:00PM</p>
-				</div>
-				<div className=" flex justify-start w-full">
-					<p>No</p>
-				</div>
-				<div className=" flex justify-end">
-					<li className="flex">
-						<p className="mr-2 detail-button flex-center">
-							<span>Details</span>
-						</p>
-						<p className="timer-icon w-6 h-8" />
-					</li>
-				</div>
-			</div>
-			<hr />
-
-			<div className="contents w-full h-screen flex justify-start items-center">
-				<div className="flex justify-start w-full">
-					<p>Write an Letter</p>
-				</div>
-				<div className="flex justify-start w-full">
-					<p>May 10, 2020</p>
-				</div>
-				<div className="flex justify-start w-full">
-					<p>May 10, 2020 1:00PM</p>
-				</div>
-				<div className="flex justify-start w-full">
-					<p className="checked-container" />
-				</div>
-				<div className=" flex justify-end">
-					<li className="flex">
-						<p className="mr-2 detail-button flex-center">
-							<span>Details</span>
-						</p>
-						<p className="delete-icon w-6 h-8" />
-					</li>
-				</div>
-			</div>
+			{Array.from([ 1, 2, 3, 4, 5, 6, 7, 8 ], (item) => (
+				<Fragment key={item}>
+					<div className="contents w-full h-screen flex justify-start items-center">
+						<div className="flex justify-start w-full">
+							<p>Write an Letter</p>
+						</div>
+						<div className="flex justify-start w-full">
+							<p>May 10, 2020</p>
+						</div>
+						<div className="flex justify-start w-full">
+							<p>May 10, 2020 1:00PM</p>
+						</div>
+						<div className="flex justify-start w-full">
+							<p className="checked-container" />
+						</div>
+						<div className=" flex justify-end">
+							<li className="flex">
+								<p className="mr-2 detail-button flex-center">
+									<span>Details</span>
+								</p>
+								<p className="delete-icon w-6 h-8" />
+							</li>
+						</div>
+					</div>
+					<hr />
+				</Fragment>
+			))}
 		</div>
 	</div>
 );
--- src/components/pages/Video.page.jsx
+++ src/components/pages/Video.page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export const VideoPage = () => (
 	<div className="mt-8 w-full">
@@ -22,61 +22,28 @@ export const VideoPage = () => (
 			</div>
 			<hr />
 
-			<div className=" contents w-full h-screen flex justify-start items-center">
-				<div className=" flex justify-start w-full">
-					<p>Chapter 1- Units & Measurement</p>
-				</div>
-				<div className=" flex justify-start w-full">
-					<p>https://youtube/vidyalaya</p>
-				</div>
+			{Array.from([ 1, 2, 3, 4, 5, 6, 7, 8 ], (item) => (
+				<Fragment key={item}>
+					<div className="contents w-full h-screen flex justify-start items-center">
+						<div className=" flex justify-start w-full">
+							<p>Chapter 1- Units & Measurement</p>
+						</div>
+						<div className=" flex justify-start w-full">
+							<p>https://youtube/vidyalaya</p>
+						</div>
 
-				<div className=" flex justify-end">
-					<li className="flex">
-						<p className="mr-2 detail-button flex-center">
-							<span>Details</span>
-						</p>
-						<p className="timer-icon w-6 h-8" />
-					</li>
-				</div>
-			</div>
-			<hr />
-
-			<div className="contents w-full h-screen flex justify-start items-center">
-				<div className="flex justify-start w-full">
-					<p>Chapter 1- Units & Measurement</p>
-				</div>
-				<div className="flex justify-start w-full">
-					<p>https://youtube/vidyalaya</p>
-				</div>
-
-				<div className=" flex justify-end">
-					<li className="flex">
-						<p className="mr-2 detail-button flex-center">
-							<span>Details</span>
-						</p>
-						<p className="delete-icon w-6 h-8" />
-					</li>
-				</div>
-			</div>
-			<hr />
-
-			<div className="contents w-full h-screen flex justify-start items-center">
-				<div className="flex justify-start w-full">
-					<p>Chapter 1- Units & Measurement</p>
-				</div>
-				<div className="flex justify-start w-full">
-					<p>https://youtube/vidyalaya</p>
-				</div>
-
-				<div className=" flex justify-end">
-					<li className="flex">
-						<p className="mr-2 detail-button flex-center">
-							<span>Details</span>
-						</p>
-						<p className="delete-icon w-6 h-8" />
-					</li>
-				</div>
-			</div>
+						<div className=" flex justify-end">
+							<li className="flex">
+								<p className="mr-2 detail-button flex-center">
+									<span>Details</span>
+								</p>
+								<p className="timer-icon w-6 h-8" />
+							</li>
+						</div>
+					</div>
+					<hr />
+				</Fragment>
+			))}
 		</div>
 	</div>
 );
